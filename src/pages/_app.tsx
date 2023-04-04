26b890acef326b22f9d2906ae3ac3d8ad7557b53
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-04-04 20:20:13 +0200

    Implement react hot toast and nextjs Head

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import { type AppType } from "next/app";
-import { type Session } from "next-auth";
+import Head from "next/head";
+import { Suspense, lazy } from "react";
+import type { AppType } from "next/app";
+import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
+
+const LazyToaster = lazy(() =>
+  import("react-hot-toast").then((module) => ({ default: module.Toaster }))
+);
 
 import { api } from "~/utils/api";
 
@@ -12,6 +18,16 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <title>Chatr</title>
+        <meta
+          name="description"
+          content="Chatr: A haven for tweets without Elongated Muskrat's influence!"
+        />
+      </Head>
+      <Suspense fallback={<div>Loading...</div>}>
+        <LazyToaster position="bottom-center" />
+      </Suspense>
       <Component {...pageProps} />
     </SessionProvider>
   );
