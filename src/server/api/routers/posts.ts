5f20c0cb3352615bbe86855a5d61a502f45e822a
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-04-06 20:40:45 +0200

    add getAll procedure

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,16 @@
 import { z } from "zod";
-import { createTRPCRouter, protectedProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 
 export const postsRouter = createTRPCRouter({
+  getAll: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.post.findMany({
+      include: {
+        author: true,
+      },
+      take: 10,
+      orderBy: { createdAt: "desc" },
+    });
+  }),
   create: protectedProcedure
     .input(z.object({ content: z.string() }))
     .mutation(({ ctx, input }) => {
