c958c5242269dd2b22a7dde682156f3d637bcb55
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-04-08 10:40:54 +0200

    Replace Image with Avatar and include LoadingSpinner

--- src/components/posts/PostForm/PostForm.tsx
+++ src/components/posts/PostForm/PostForm.tsx
@@ -1,5 +1,4 @@
 import { z } from "zod";
-import Image from "next/image";
 import { toast } from "react-hot-toast";
 import type { Post, User } from "@prisma/client";
 import { useSession } from "next-auth/react";
@@ -10,7 +9,7 @@ import { v4 as uuidv4 } from "uuid";
 import { api } from "~/utils/api";
 import useZodForm from "~/utils/hooks/useZodForm";
 
-import { CharacterCount } from "~/components/common";
+import { Avatar, CharacterCount, LoadingSpinner } from "~/components/common";
 
 const MediaButtons = () => {
   return (
@@ -27,23 +26,6 @@ const MediaButtons = () => {
       <button className="inline-flex w-full justify-center rounded-full border border-gray-300 bg-transparent px-5 py-2 text-sm font-semibold text-gray-800  transition-colors hover:bg-gray-200">
         Poll
       </button>
-    </div>
-  );
-};
-
-export const LoadingSpinner = () => {
-  return (
-    <div aria-label="Loading..." role="status">
-      <svg className="h-3 w-3 animate-spin" viewBox="3 3 18 18">
-        <path
-          className="fill-gray-200"
-          d="M12 5C8.13401 5 5 8.13401 5 12c0 3.866 3.13401 7 7 7 3.866.0 7-3.134 7-7 0-3.86599-3.134-7-7-7zM3 12c0-4.97056 4.02944-9 9-9 4.9706.0 9 4.02944 9 9 0 4.9706-4.0294 9-9 9-4.97056.0-9-4.0294-9-9z"
-        ></path>
-        <path
-          className="fill-gray-800"
-          d="M16.9497 7.05015c-2.7336-2.73367-7.16578-2.73367-9.89945.0-.39052.39052-1.02369.39052-1.41421.0-.39053-.39053-.39053-1.02369.0-1.41422 3.51472-3.51472 9.21316-3.51472 12.72796.0C18.7545 6.02646 18.7545 6.65962 18.364 7.05015c-.390599999999999.39052-1.0237.39052-1.4143.0z"
-        ></path>
-      </svg>
     </div>
   );
 };
@@ -132,12 +114,18 @@ const PostForm = () => {
     <div className="flex w-full items-start gap-4">
       <div className="flex grow flex-col">
         <div className="flex gap-4 rounded-xl bg-gray-100 p-4 shadow-sm">
-          <Image
+          {/*         <Image
             className="flex h-12 w-12 items-center justify-center rounded-full"
             src={sessionData?.user?.image ?? ""}
             alt={sessionData?.user?.name ?? ""}
             width={48}
             height={48}
+          /> */}
+
+          <Avatar
+            size="lg"
+            label={sessionData?.user.name ?? ""}
+            imageUrl={sessionData?.user?.image ?? ""}
           />
           <form
             onSubmit={handleSubmit(onSubmit)}
