beb913f5ce747ae0ab7eb57d061ebe5795325af5
turkonjavladimir <vladimir.turkonja@icloud.com>
2023-04-15 15:18:35 +0200

    extend LoadingSpinner with custom sizes

--- src/components/common/LoadingSpinner/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner/LoadingSpinner.tsx
@@ -1,7 +1,35 @@
-const LoadingSpinner = () => {
+import type { HTMLAttributes } from "react";
+import { type VariantProps, cva } from "class-variance-authority";
+
+const loadingSpinner = cva("animate-spin", {
+  variants: {
+    size: {
+      xs: ["w-3", "h-3"],
+      sm: ["w-5", "h-5"],
+      md: ["w-8", "h-8"],
+      lg: ["w-10", "h-10"],
+    },
+  },
+  defaultVariants: {
+    size: "xs",
+  },
+});
+
+type LoadingSpinnerProps = {
+  label?: string;
+  imageUrl?: string;
+} & HTMLAttributes<HTMLOrSVGElement> &
+  VariantProps<typeof loadingSpinner>;
+
+const LoadingSpinner = ({ size }: LoadingSpinnerProps) => {
   return (
     <div aria-label="Loading..." role="status">
-      <svg className="h-3 w-3 animate-spin" viewBox="3 3 18 18">
+      <svg
+        className={`${loadingSpinner({
+          size,
+        })}`}
+        viewBox="3 3 18 18"
+      >
         <path
           className="fill-gray-200"
           d="M12 5C8.13401 5 5 8.13401 5 12c0 3.866 3.13401 7 7 7 3.866.0 7-3.134 7-7 0-3.86599-3.134-7-7-7zM3 12c0-4.97056 4.02944-9 9-9 4.9706.0 9 4.02944 9 9 0 4.9706-4.0294 9-9 9-4.97056.0-9-4.0294-9-9z"
